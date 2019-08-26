fca974cd7ef461914586deedc1d77e4ed2c5f63b
Damon Jenkins <damonsfjenkins@gmail.com>
2019-08-26 14:53:38 +1200

    Updated Vector2, Added Camera Position-Targeting

--- Splatformer/Camera.h
+++ Splatformer/Camera.h
@@ -6,6 +6,8 @@ public:
 	Camera() = default;
 	Camera(float width, float height);
 
+	void Update();
+
 	void SetPosition(Vector2 newPosition);
 	Vector2 GetPosition();
 
@@ -15,11 +17,21 @@ public:
 	void SetHeight(float newHeight);
 	float GetHeight();
 
+	void SetTargetPosition(Vector2 newTarget);
+	Vector2 GetTargetPosition();
+
+	void SetMoveSpeed(float newMoveSpeed);
+	float GetMoveSpeed();
+
 	SDL_Rect* GetRect();
 
 private:
 	float width, height;
 	Vector2 position = {0.0f, 0.0f};
+
+	Vector2 targetPosition = { 0.0f, 0.0f };
+	//The number of pixels to move per second toward the target position
+	float moveSpeed = 0.0f;
 
 	SDL_Rect drawRect;
 };
